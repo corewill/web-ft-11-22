fc777511254d5ca8f8f2a30ed003da5dcce09e43
corewill <118204159+corewill@users.noreply.github.com>
2022-11-28 10:08:52 -0500

    tippy

---
+++ Week1/week2/Day3/Game1.py
@@ -0,0 +1,30 @@
+class Character:
+    def __init__ (self, engage, att, dfn, runaway, health):
+        self.att 
+        self.dfn
+        self.runaway
+        self.health
+
+    def hit(self, att):
+        while True:
+            if self.dodge >= attack:
+                print("dodged attack")
+                return False
+            newHealth = self.defense - attack
+            print(f'your health is{newHealth}')
+            if  newHealth <= 0:
+                print('wasted')
+                return False
+
+    def block(self, dfn):
+         while True:
+            if self.dodge >= attack:
+                print("dodged attack")
+                return False
+            newHealth = self.defense - attack
+            print(f'your health is{newHealth}')
+            if  newHealth <= 0:
+                print('wasted')
+                return False
+
+    def disingage(self, runaway):
